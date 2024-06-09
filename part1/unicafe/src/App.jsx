e17acd43215b60c4c689887e6c6fc1f362dc4ba5
div-dev <divyanshchawla12@gmail.com>
2024-06-10 03:39:03 +0530

    1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const Display = (props) =>{
+  return (
+    <div>
+      {props.text} ={'>'} {props.value}
+    </div>
+  )
+}
+
+
+
+const Button = (props) =>{
+  return(
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+
+  return (
+    <div>
+      <h1>Give FeedBack</h1>
+      <Button handleClick={()=> setGood(good+1)} text='good'/>
+      <Button handleClick={()=> setNeutral(neutral+1)} text='neutral'/>
+      <Button handleClick={()=> setBad(bad+1)} text='bad'/>
+      
+      
+      <h1>Statistics</h1>
+      <Display text='good' value={good}/>
+      <Display text='nuetral' value={neutral}/>
+      <Display text='bad' value={bad}/>
+
+      
+    </div>
+  )
+}
+
+export default App
